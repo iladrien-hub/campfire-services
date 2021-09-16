31df41bbcfcaca9da3d33ed2e03b054a48a86fed
iladrien-hub <sliptchenko71@gmail.com>
2021-09-16 11:00:18 +0300

    chore(BlogService) basic config

--- UserService/UserService/settings.py
+++ UserService/UserService/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-c%-c^%t-8dkdo@q#q+&9n#b@89_li66-jb10e(2@mtzpccoijn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
