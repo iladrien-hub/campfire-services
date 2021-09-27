f92187362b94028771dcdeb2831e13b83fb660a3
iladrien-hub <sliptchenko71@gmail.com>
2021-09-27 16:03:26 +0300

    feature(BlogService.ArticleCreationSerializer) add Image field

--- BlogService/article/api/v0/serializers.py
+++ BlogService/article/api/v0/serializers.py
@@ -1,19 +1,23 @@
 from rest_framework import serializers
 
-from article.models import Article
+from article.models import Article, ArticleImage
 
 
 class ArticleCreationSerializer(serializers.ModelSerializer):
+    image = serializers.ListField(required=False)
 
     class Meta:
         model = Article
-        fields = ['title', 'text']
+        fields = ['text', 'image']
 
     def save(self, user):
         article = Article(
-            title=self.validated_data['title'],
             text=self.validated_data['text'],
             user=user
         )
         article.save()
+        for image in self.validated_data.get('image', ()):
+            article_image = ArticleImage(model=article)
+            article_image.set_image(image)
+            article_image.save()
         return article
