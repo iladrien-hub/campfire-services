2b87eda49e8efe41107ba08926c4774f7ca50372
iladrien-hub <sliptchenko71@gmail.com>
2021-09-28 13:04:16 +0300

    feature(ArticleViewSet) create ArticleViewSet

--- BlogService/article/api/v0/urls.py
+++ BlogService/article/api/v0/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from article.api.v0.views import article_create_view
+from article.api.v0 import views
 
 app_name = 'article'
 
 
 urlpatterns = [
-    path('create', article_create_view, name='create'),
+    path('create', views.article_create_view, name='create'),
+    path('retrieve/<int:pk>', views.ArticleViewSet.as_view(actions={"get": "retrieve"}), name='retrieve'),
 ]
